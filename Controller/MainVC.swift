3eb4a1f5ff91415700eb2555bd71fbcc2c7f65e5
Jackson <jacksonmai@gmail.com>
2019-11-01 22:16:10 -0500

    input accessory

--- Controller/MainVC.swift
+++ Controller/MainVC.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageText: CurrencyTextField!
+    @IBOutlet weak var priceText: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 75))
+        calcButton.backgroundColor = #colorLiteral(red: 0.8472951055, green: 0.4272649618, blue: 0.1943883885, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageText.inputAccessoryView = calcButton
+        priceText.inputAccessoryView = calcButton
+        
     }
 
+    @objc func calculate() {
+        print("We got here")
+    }
 
 }
 
